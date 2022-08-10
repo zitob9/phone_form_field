3f244e0068279d798666fa06a34f7ab98a444ac9
cedvdb <cedvandenbosch@gmail.com>
2022-08-10 17:41:01 +0200

    base href

--- build.sh
+++ build.sh
@@ -1,1 +1,1 @@
-rm docs -rf && cd example && flutter build web --release && cd .. && mkdir docs && mv example/build/web/* docs
+rm docs -rf && cd example && flutter build web --release --base-href "/phone_form_field/" && cd .. && mkdir docs && mv example/build/web/* docs
